5b25409c941cb7aef83f61cd6d8b5b2393a4e8d2
Irsa Cahaya Widodo <irsacahaya17@gmail.com>
2025-02-18 11:20:28 +0700

    Pratikum 1 Telah Selesai

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Nama: Irsa Cahaya Widodo <br> NIM: 2341720193';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya  '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='Jhon') {
+    return 'Nama saya  '.$name;
 });
